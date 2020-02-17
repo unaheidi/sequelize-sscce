53faab438b65998aa591b301ddeee5859a06142f
papb <papb1996@gmail.com>
2020-02-16 21:25:31 -0300

    update

--- setup/travis-ci-helper.sh
+++ setup/travis-ci-helper.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -c
 
+if [ $USE_TS ]; then
+    npm i;
+else
+    npm i --production;
+fi
+
 if [ "$DIALECT" = "postgres" ]; then
     npm i pg@^7 pg-hstore@^2 pg-types@^2;
 elif [ "$DIALECT" = "mysql" ]; then
